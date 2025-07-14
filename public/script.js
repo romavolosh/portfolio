8150c41282f553522c4dc6f2d5f9fd918e912799
romavolosh <romavolosh@gmail.com>
2025-07-14 11:26:25 +0300

    GitHub Api

--- public/script.js
+++ public/script.js
@@ -1,5 +1,39 @@
 import interact from "https://cdn.interactjs.io/v1.9.20/interactjs/index.js";
 
+const apiUrl = 'https://api.github.com/users/romavolosh';
+
+fetch(apiUrl)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log(data);
+    var gitUrl = document.getElementById('git_url');
+    gitUrl.textContent = data.html_url;
+    var gitUsername = document.getElementById('git_username');
+    gitUsername.textContent = data.login;
+    var gitBio = document.getElementById('git_bio');
+    gitBio.textContent = data.bio || 'No bio available';
+    var gitRepos = document.getElementById('git_repos');
+    gitRepos.textContent = data.public_repos || 'No public repositories';
+    var gitFollowers = document.getElementById('git_followers');
+    gitFollowers.textContent = data.followers || 'No followers';
+    var gitFollowing = document.getElementById('git_following');
+    gitFollowing.textContent = data.following || 'Not following anyone';
+    var gitName = document.getElementById('git_name');
+    gitName.textContent = data.name || 'No name available';
+    var gitLocation = document.getElementById('git_location');
+    gitLocation.textContent = data.location || 'No location available';
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+
+
+
 function dragMoveListener(event) {
   var target = event.target;
   var x = (parseFloat(target.getAttribute("data-x")) || 0) + event.dx;
@@ -134,6 +168,47 @@ interact('.settingsWindow')
     ],
   });
 
+interact(".githubWindow")
+  .resizable({
+    edges: { left: true, right: true, bottom: true, top: true },
+    listeners: {
+      move(event) {
+        var target = event.target;
+        var x = parseFloat(target.getAttribute("data-x")) || 0;
+        var y = parseFloat(target.getAttribute("data-y")) || 0;
+
+        target.style.width = event.rect.width + "px";
+        target.style.height = event.rect.height + "px";
+
+        x += event.deltaRect.left;
+        y += event.deltaRect.top;
+
+        target.style.transform = "translate(" + x + "px," + y + "px)";
+        target.setAttribute("data-x", x);
+        target.setAttribute("data-y", y);
+      },
+    },
+    modifiers: [
+      interact.modifiers.restrictEdges({
+        outer: "body",
+      }),
+      interact.modifiers.restrictSize({
+        min: { width: 350, height: 150 },
+      }),
+    ],
+    inertia: true,
+  })
+  .draggable({
+    listeners: { move: window.dragMoveListener },
+    inertia: true,
+    modifiers: [
+      interact.modifiers.restrictRect({
+        restriction: "body",
+        endOnly: true,
+      }),
+    ],
+  });
+
 document.getElementById("terminalBtn").addEventListener("click", function () {
   const terminal = document.querySelector(".terminalWindow");
   const taskbarBtn = document.querySelector(".taskbarBtn");
@@ -174,6 +249,20 @@ document.getElementById('contactClose').addEventListener('click', function () {
   const contactBtn = document.getElementById('contactBtn');
   if (contactWindow) contactWindow.style.display = 'none';
   if (contactBtn) contactBtn.style.backgroundColor = 'transparent';
+});
+
+document.getElementById("githubBtn").addEventListener("click", function () {
+  const githubWindow = document.querySelector(".githubWindow");
+  const taskbarBtn = document.getElementById("githubBtn");
+  if (githubWindow) {
+    const isVisible = githubWindow.style.display === "block";
+    githubWindow.style.display = isVisible ? "none" : "block";
+    if (taskbarBtn) {
+      taskbarBtn.style.backgroundColor = isVisible
+        ? "transparent"
+        : "rgba(255, 255, 255, 0.205)";
+    }
+  }
 });
 
 document
